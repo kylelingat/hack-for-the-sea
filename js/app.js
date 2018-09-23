6a98f4f150782f5bed12e74ca0fc213a37480d89
ageronimo <angelageroniimo@gmail.com>
2018-09-22 22:20:26 -1000

    Menu feature functional, needs styling

--- js/app.js
+++ js/app.js
@@ -3,44 +3,62 @@ console.log("hello");
 var map, infowindow;
 
 function initMap() {
-        map = new google.maps.Map(document.getElementById('map'), {
-          center: {
-            lat: 21.3882887,
-            lng: -157.9885406
-          },
-          zoom: 10,
-          mapTypeId : "satellite",
-        });
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: {
+      lat: 21.3882887,
+      lng: -157.9885406
+    },
+    zoom: 10,
+    mapTypeId: "satellite",
+  });
 
-        infoWindow = new google.maps.InfoWindow;
+  infoWindow = new google.maps.InfoWindow;
 
-        if (navigator.geolocation) {
-          navigator.geolocation.getCurrentPosition(function(position) {
-            var pos = {
-              lat: position.coords.latitude,
-              lng: position.coords.longitude
-            };
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function (position) {
+      var pos = {
+        lat: position.coords.latitude,
+        lng: position.coords.longitude
+      };
 
-            infoWindow.setPosition(pos);
-            infoWindow.setContent('Location found.');
-            infoWindow.open(map);
-            map.setCenter(pos);
-          }, function() {
-            handleLocationError(true, infoWindow, map.getCenter());
-          });
-        } else {
-          // Browser doesn't support Geolocation
-          handleLocationError(false, infoWindow, map.getCenter());
-        }
-      }
+      infoWindow.setPosition(pos);
+      infoWindow.setContent('Location found.');
+      infoWindow.open(map);
+      map.setCenter(pos);
+    }, function () {
+      handleLocationError(true, infoWindow, map.getCenter());
+    });
+  } else {
+    // Browser doesn't support Geolocation
+    handleLocationError(false, infoWindow, map.getCenter());
+  }
+}
 
-      function handleLocationError(browserHasGeolocation, infoWindow, pos) {
-        infoWindow.setPosition(pos);
-        infoWindow.setContent(browserHasGeolocation ?
-                              'Error: The Geolocation service failed.' :
-                              'Error: Your browser doesn\'t support geolocation.');
-        infoWindow.open(map);
-    }
+function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+  infoWindow.setPosition(pos);
+  infoWindow.setContent(browserHasGeolocation ?
+    'Error: The Geolocation service failed.' :
+    'Error: Your browser doesn\'t support geolocation.');
+  infoWindow.open(map);
+}
 
-  //onclick openMenu
-  //change grid class to activemenu/grid or inactivemenu/grid
+//open and close mobile menu
+  let menuCount = 0;
+$('#openMenu').on('click', () => {
+  console.log('hellooo');
+  menuCount++;
+
+  if (x % 2 !== 0) {
+    $('#openMenu').removeClass('activeMenu');
+    $('#openMenu').addClass('inactiveMenu');
+
+    $('#pageContainer').removeClass('activeGrid');
+    $('#pageContainer').addClass('inactiveGrid');
+  } else {
+    $('#openMenu').addClass('activeMenu');
+    $('#openMenu').removeClass('inactiveMenu');
+
+    $('#pageContainer').addClass('activeGrid');
+    $('#pageContainer').removeClass('inactiveGrid');
+  }
+})
